a92e9e196985681864f83c10ba182d1bb736c14e
Artem Sultanov <79450613+ArtemSultanov@users.noreply.github.com>
2022-11-24 11:12:09 +0200

    Update 3-Program-Сlasses-Example.py

--- 3- SoftwareConstruction/3-Program-Сlasses-Example.py
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example.py
@@ -1,52 +1,52 @@
 class Consumer:
-    def __init__(selfesteem, name, surname, consumer_id, credit_card):
-        selfesteem.name = name
-        selfesteem.surname = surname
-        selfesteem.consumer_id = consumer_id
-        selfesteem.credit_card = credit_card
+    def __init__(self, name, surname, consumer_id, credit_card):
+        self.name = name
+        self.surname = surname
+        self.consumer_id = consumer_id
+        self.credit_card = credit_card
 
-    def name_change(selfesteem):
-        selfesteem.name = input("Введіть нове ім'я: ")
-        return selfesteem.name
+    def name_change(self):
+        self.name = input("Введіть нове ім'я: ")
+        return self.name
 
 
 class Courses:
-    def __init__(selfesteem, course_name):
-        selfesteem.course_name = course_name
-        selfesteem.lesson_name = "null"
-        selfesteem.course_date = 0
-        selfesteem.time = 0
+    def __init__(self, course_name):
+        self.course_name = course_name
+        self.lesson_name = "null"
+        self.course_date = 0
+        self.time = 0
 
-    def create_lesson(selfesteem):
-        lesson_name = input("Введіть назву курсу")
+    def create_lesson(self):
+        self = input("Введіть назву курсу")
         course_date = input("Дата курсу")
         return course_date, lesson_name
 
-    def time_to_lesson(selfesteem, current_time):
+    def time_to_lesson(self, current_time):
         time_to_les = current_time - selfesteem.time
         return time_to_les
 
-    def data_change(selfesteem):
-        selfesteem.course_date = input("Нова дата курсу")
-        return selfesteem.course_date
+    def data_change(self):
+        self.course_date = input("Нова дата курсу")
+        return self.course_date
 
-    def time_change(selfesteem):
-        selfesteem.time = input("Новий час курсу")
-        return selfesteem.time
+    def time_change(self):
+        self.time = input("Новий час курсу")
+        return self.time
 
-    def lesson_change(selfesteem):
-        selfesteem.lesson_name = input("Нова назва курсу")
-        return selfesteem.lesson_name
+    def lesson_change(self):
+        self.lesson_name = input("Нова назва курсу")
+        return self.lesson_name
 
-    def course_info(selfesteem):
-        return selfesteem.course_name, selfesteem.course_date, selfesteem.lesson_name, selfesteem.time
+    def course_info(self):
+        return self.course_name, self.course_date, self.lesson_name, self.time
 
-    def machine_check(selfesteem):
-        return selfesteem.course_info
+    def machine_check(self):
+        return self.course_info
 
 
 class Lector(Consumer):
 
-    def create_description(selfesteem):
+    def create_description(self):
         description = input("Введіть опис курсу: ")
         return print(description)
