dfad51b94e8948652f37c08bd9216df3e50c3382
Artem Sultanov <79450613+ArtemSultanov@users.noreply.github.com>
2022-11-24 19:22:42 +0200

    Create 5-Program-Methods-Using-Example.py

---
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    Example1 = Courses("Introducing in Python") # Cтворюємо об'єкт Example1
+    Example1.find_lesson() # шукаємо заняття, вводячи після визову фунції значення змінних
