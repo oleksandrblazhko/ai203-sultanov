c93919ec50f383622ff03594a1d6d13c961bc637
Artem Sultanov <79450613+ArtemSultanov@users.noreply.github.com>
2022-11-24 10:54:06 +0200

    Create 5-Program-Methods-Using-Example.py

---
+++ 3- SoftwareConstruction/5-Program-Methods-Using-Example.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    Example1 = Courses("Introducing in Python") # Cтворюємо об'єкт Example1
+    Example1.create_lesson() # створюємо заняття, вводячи після визову фунції значення змінних
+    Example1.time_to_lesson(10.55) # визначаемо час до заняття задаючі за поточне 10.55
